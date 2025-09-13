6b35aee75167526bebbc81c26872998498f8f752
Mitesh Goyal <miteshgoyal.00@gmail.com>
2025-09-13 16:34:28 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
     eslint: {
         ignoreDuringBuilds: true,
     },
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: "https",
